e61ca64fc1c0e6463d37315a6e50982771a9947b
Hemanth Sai <gurlahemanth.2005@gmail.com>
2026-01-11 16:59:07 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import {Toaster} from "react-hot-toast"
+import { AuthContext } from '../context/AuthContext'
+
+const App = () => {
+  const { authUser } = useContext(AuthContext)
+  return (
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster/>
+      <Routes>
+         <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
+         <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
+         <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App                                                                                                                                                                                                                                                                                                                                                  
+
+// const App = () => {
+//   const { authUser } = useContext(AuthContext)
+//   return (
+//     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+//       <Toaster/>
+//       <Routes>
+//         <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
+//         <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
+//         <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
+//       </Routes>
+//     </div>
+//   )
+// }
+
+// export default App                                                                                                                                                                                                                                                                                                                                                  
+
+
+
+// import React, { useContext } from "react";
+// import { Routes, Route } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+// import LoginPage from "./pages/LoginPage";
+// import ProfilePage from "./pages/ProfilePage";
+// import { AuthContext } from "../context/AuthContext";
+
+// const App = () => {
+//   const { authUser } = useContext(AuthContext);
+
+//   return (
+//     <div className="min-h-screen bg-white">
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/profile" element={<ProfilePage />} />
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+// const App = () => {
+//   return <h1>APP IS RENDERING</h1>;
+// };
+
+// export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from '../context/AuthContext.jsx'
+import { ChatProvider } from '../context/ChatContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <AuthProvider>
+    <ChatProvider>
+      <App />
+    </ChatProvider>  
+  </AuthProvider>
+  </BrowserRouter>
+) 
